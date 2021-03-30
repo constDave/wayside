3a3de1aa0a4045176f590b19ea58478ee2b256fe
DAVID ENRIQUEZ <const.dave@gmail.com>
2021-03-30 13:43:06 -0500

    remove references to moved images

--- js/resize.js
+++
@@ -1,9 +0,0 @@
-const sharp = require('sharp');
-const fs = require('fs');
-const directory = '../images';
-
-fs.readdirSync(directory).forEach(file => {
-  sharp(`${directory}/${file}`)
-    .resize(500, 500) // width, height
-    .toFile(`${directory}/${file}-small.jpg`);
-  });
